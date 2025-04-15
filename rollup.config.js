d825ce322a6d0dcd041eed2dea15eea7c435a8bd
Arifur Rahman <113544876+antor-arif@users.noreply.github.com>
2025-04-15 23:58:18 +0600

    Fixed issue in backend module

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import  terser  from '@rollup/plugin-terser';
+import json from '@rollup/plugin-json'; 
 
 export default {
     input: 'src/index.js',
@@ -14,5 +15,5 @@ export default {
             format: 'cjs',
         },
     ],
-    plugins: [resolve(), commonjs(), terser()],
+    plugins: [resolve(), commonjs(), terser(), json()],
 };
